2745273fa46d1675bce1a4f70b0419eaed8f90f9
alep-2000 <alessiopiras056@gmail.com>
2023-06-07 15:45:03 +0200

    Bonus finisched

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
-            message: '<h1>Hello world!!</h1>'
+            message: '<h1>Hello world!!</h1>',
+            image: 'https://picsum.photos/200/300'
         }
     }
 }).mount('#app');
